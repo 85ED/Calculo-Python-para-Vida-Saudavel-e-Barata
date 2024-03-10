cd5c2e6f382e0eccb6eb133a2c0d71f41474977a
Edson Felix <160767641+85ED@users.noreply.github.com>
2024-03-10 12:10:36 -0300

    calc_VidaBoa.py

--- calc_VidaBoa.py
+++ calc_VidaBoa.py
@@ -5,7 +5,7 @@ volumedia = float(input("Quantos maços de cigarros você fuma por dia?: "))
 
 #calculos:
 valor_gasto = float(tempo) * 365 * (customaco) * (volumedia)
-if valor_gasto <= 20000:
+if valor_gasto < 20000:
     print("Com o valor R$ {:.2f},você poderia ter dado entrada em um carro.".format(valor_gasto))
 elif valor_gasto <=50000:
     print("Com o valor R$ {:.2f}, você poderia ter comprado um carro popular usado.".format(valor_gasto))
